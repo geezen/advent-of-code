086693aba0c5b8ce4c141f982c5b69c83a5c1c3c
marcusgerhardsson <gerhardsson.marcus@gmail.com>
2025-12-01 09:35:37 +0100

    2025 Day1 complete

--- 2025/Day1/Day1.py
+++ 2025/Day1/Day1.py
@@ -1,12 +1,23 @@
+# Parse input to (direction, distance) touple
+rotations = [(1, int(line[1:])) if line[0] == 'R' else (-1, int(line[1:])) for line in open("2025/Day1/in.txt").readlines()]
+
+# Part 1
 dial = 50
 password = 0
-
-for line in open("2025/Day1/in.txt").readlines():
-    sign = -1 if line[0] == 'L' else 1
-    dist = int(line[1:])
-    rotation = sign * dist
-    dial += rotation
+for dir, dist in rotations:
+    dial += dir * dist
     if dial % 100 == 0:
         password += 1
 
 print(f"Part1: {password}")
+
+# Part 2
+dial = 50
+password = 0
+for dir, dist in rotations:
+    for i in range(dist):
+        dial += dir
+        if dial % 100 == 0:
+            password += 1
+
+print(f"Part2: {password}")
