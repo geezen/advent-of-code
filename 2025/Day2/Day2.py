ce2bea429b7ee494fd8e9e03456ad464cfa0238f
marcusgerhardsson <gerhardsson.marcus@gmail.com>
2025-12-02 08:54:35 +0100

    2025 Day2 precompile regex

--- 2025/Day2/Day2.py
+++ 2025/Day2/Day2.py
@@ -4,11 +4,13 @@ ranges = [tuple(map(int, range.split("-"))) for range in open("2025/Day2/in.txt"
 
 res1 = 0
 res2 = 0
+pattern1 = re.compile(r"(\d+)\1")
+pattern2 = re.compile(r"(\d+)\1+")
 for ids in ranges:
     for id in range(ids[0], ids[1] + 1):
         s = str(id)
-        if re.fullmatch(r"(\d+)\1", s):
+        if pattern1.fullmatch(s):
             res1 += id
-        if re.fullmatch(r"(\d+)\1+", s):
+        if pattern2.fullmatch(s):
             res2 += id
 print(f"part1: {res1}\npart2: {res2}")
