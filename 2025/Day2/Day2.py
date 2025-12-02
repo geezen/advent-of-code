31e09bdf46ed2901c5666bfb4c6fea53ed17cb6b
marcusgerhardsson <gerhardsson.marcus@gmail.com>
2025-12-02 08:45:15 +0100

    2035 Day2 complete

--- 2025/Day2/Day2.py
+++ 2025/Day2/Day2.py
@@ -2,10 +2,13 @@ import re
 
 ranges = [tuple(map(int, range.split("-"))) for range in open("2025/Day2/in.txt").read().split(",")]
 
-res = 0
+res1 = 0
+res2 = 0
 for ids in ranges:
     for id in range(ids[0], ids[1] + 1):
         s = str(id)
-        if re.fullmatch(r"(\d+)\1$", s):
-            res += id
-print(res)
+        if re.fullmatch(r"(\d+)\1", s):
+            res1 += id
+        if re.fullmatch(r"(\d+)\1+", s):
+            res2 += id
+print(f"part1: {res1}\npart2: {res2}")
