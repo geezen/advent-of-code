57ca3ed013a19e049d1676cfebb5abbdcab390e5
marcusgerhardsson <gerhardsson.marcus@gmail.com>
2025-12-03 09:59:25 +0100

    2025 Day3 complete

--- 2025/Day3/Day3.py
+++ 2025/Day3/Day3.py
@@ -2,6 +2,7 @@ import re
 
 banks = [list(map(int, re.findall(r"\d", line))) for line in open("2025/Day3/in.txt").readlines()]
 
+# part1  
 res = 0
 for bank in banks:
     digit1_index = 0
@@ -15,3 +16,22 @@ for bank in banks:
         digit2_val = max(digit2_val, bank[i])
     res += 10*digit1_val + digit2_val
 print(f"part1: {res}")
+
+# part 2
+def findjoltage(bank, nbats):
+    joltage = 0
+    prevdigit_index = -1
+    for right in range(nbats, 0, -1):
+        maxdigit_val = 0
+        for i in range(prevdigit_index + 1, len(bank) - right + 1):
+            if bank[i] > maxdigit_val:
+                maxdigit_val = bank[i]
+                prevdigit_index = i
+        joltage *= 10
+        joltage += maxdigit_val
+    return joltage
+
+res = 0
+for bank in banks:
+    res += findjoltage(bank, 12)
+print(f"part2: {res}")
