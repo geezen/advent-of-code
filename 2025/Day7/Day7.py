027418a78de1e9d74405f3ab114acb5b2f787441
marcusgerhardsson <gerhardsson.marcus@gmail.com>
2025-12-07 15:26:10 +0100

    2025 Day7 complete

--- 2025/Day7/Day7.py
+++ 2025/Day7/Day7.py
@@ -1,14 +1,16 @@
-from collections import Counter
-
-inp = open("2025/Day7/in.txt").read().splitlines()
-nextbeams = [1 if i == inp[0].index('S') else 0 for i in range(len(inp[0]))]
+inp = open("2025/Day7/example.txt").read().splitlines()
+nextbeams = [1 if c == 'S' else 0 for c in inp[0]]
+splits = 0
 for line in inp[1:]:
     currentbeams = nextbeams
     nextbeams = [0 for _ in range(len(inp[0]))]
     for i, count in enumerate(currentbeams):
+        if count == 0: continue
         if line[i] == '^':
             nextbeams[i - 1] += count
             nextbeams[i + 1] += count
+            splits += 1
         else:
             nextbeams[i] += count
-print(sum(nextbeams))
+print(f"Part 1: {splits}")
+print(f"Part 2: {sum(nextbeams)}")
