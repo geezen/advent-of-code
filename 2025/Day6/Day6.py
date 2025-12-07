5e35e619e272eef60653d6f0bb3ea017a2b76fb9
marcusgerhardsson <gerhardsson.marcus@gmail.com>
2025-12-07 13:49:39 +0100

    2025 Day6 complete

--- 2025/Day6/Day6.py
+++ 2025/Day6/Day6.py
@@ -1,11 +1,33 @@
-input = [line.split() for line in open("2025/Day6/in.txt").readlines()]
+from functools import reduce
+from operator import add, mul
+
+lines = open("2025/Day6/example.txt").read().splitlines()
+operators = {'+': add, '*': mul}
 
 # Part 1
-accumulator = [1 if i == '*' else 0 for i in input[-1]]
-for row in input[:-1]:
+grid = [line.split() for line in lines]
+accumulator = [1 if i == '*' else 0 for i in grid[-1]]
+for row in grid[:-1]:
     for i, el in enumerate(row):
-        if input[-1][i] == '*':
+        if grid[-1][i] == '*':
             accumulator[i] *= int(el)
         else:
             accumulator[i] += int(el)
-print(sum(accumulator))
+print(f"Part 1: {sum(accumulator)}")
+
+# Part 2
+inv_grid = []
+sum = 0
+for i in reversed(range(len(lines[0]))):
+    s = ""
+    for j in range(len(lines)):
+        s += lines[j][i]
+    if not s.isspace():
+        inv_grid.append(s)
+nbrs = []
+for line in inv_grid:
+    nbrs.append(int(line[:-1]))
+    if line[-1] != ' ':
+        sum += reduce(operators[line[-1]], nbrs)
+        nbrs = []
+print(f"Part 2: {sum}")
