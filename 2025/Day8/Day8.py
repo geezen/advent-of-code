02b60cb8c95fc4d6c08acf1559b14c4e52fdd806
marcusgerhardsson <gerhardsson.marcus@gmail.com>
2025-12-08 20:19:02 +0100

    2025 Day8 cleanup

--- 2025/Day8/Day8.py
+++ 2025/Day8/Day8.py
@@ -1,12 +1,10 @@
-import re
-import heapq
 from math import sqrt
-from collections import defaultdict
 
-INPUT_PATH = "2025/Day8/in.txt"
-PART1_ITERATIONS = 1000
+INPUT_PATH = "2025/Day8/example.txt"
+PART1_ITERATIONS = 10
 
-boxes = [tuple(map(int, re.findall(r"\d+", line))) for line in open(INPUT_PATH).readlines()]
+boxes = [tuple(map(int, line.split(','))) for line in open(INPUT_PATH).readlines()]
+
 links = []
 for box1 in boxes:
     for box2 in boxes:
@@ -14,22 +12,21 @@ for box1 in boxes:
         dist = sqrt(sum([(x - y) ** 2 for x, y in zip(box1, box2)]))
         links.append((dist, box1, box2))
 
-heapq.heapify(links)
+links.sort()
 iterations = 0
 circuits = [{box} for box in boxes]
 while len(circuits) > 1:
     iterations += 1
-    dist, box1, box2 = heapq.heappop(links)
-    box1circuit = list(filter(lambda s: box1 in s, circuits))[0]
-    box2circuit = list(filter(lambda s: box2 in s, circuits))[0]
+    dist, box1, box2 = links.pop(0)
+    box1circuit = next(filter(lambda s: box1 in s, circuits))
+    box2circuit = next(filter(lambda s: box2 in s, circuits))
     if box1circuit != box2circuit:
         box1circuit.update(box2circuit)
         circuits.remove(box2circuit)
     if iterations == PART1_ITERATIONS:
         circuits.sort(key=lambda s: -1*len(s))
         ans = 1
-        for circuit in circuits[0:3]:
-            print(len(circuit))
+        for circuit in circuits[:3]:
             ans *= len(circuit)
         print(f"Part 1: {ans}")
 print(f"Part 2: {box1[0] * box2[0]}")
