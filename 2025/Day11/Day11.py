3fa9744a8a5a59594e3dccc4b780d0a0a1e42212
marcusgerhardsson <gerhardsson.marcus@gmail.com>
2025-12-11 15:09:01 +0100

    2025 Day11 complete

--- 2025/Day11/Day11.py
+++ 2025/Day11/Day11.py
@@ -1,15 +1,22 @@
 import re
+from functools import cache
 
 outputs = {}
 for line in open("2025/Day11/in.txt").readlines():
     li = list(re.findall(r"\w+", line))
     outputs[li[0]] = set(li[1:])
 
-def pathsfrom(node):
-    if node == 'out': return 1
+@cache
+def pathlength(frm, to):
+    if frm == to: return 1
+    if frm not in outputs: return 0
     ans = 0
-    for child in outputs[node]:
-        ans += pathsfrom(child)
+    for child in outputs[frm]:
+        ans += pathlength(child, to)
     return ans
 
-print(pathsfrom('you'))
+print(f"Part 1: {pathlength('you','out')}")
+
+path1length = pathlength('svr', 'fft') * pathlength('fft', 'dac') * pathlength('dac', 'out')
+path2length = pathlength('svr', 'dac') * pathlength('dac', 'fft') * pathlength('fft', 'out')
+print(f"Part 2: {path1length + path2length}")
